7d0440f650c6dbff4264aa6018b7997beb90805f
abdullahiolajide <igeabdullahiolajide@gmail.com>
2025-08-31 11:31:18 +0100

    backend url

--- client/global.js
+++ client/global.js
@@ -1,4 +1,4 @@
 //  const backendurl = 'https://ordorra-x6j9.onrender.com/api'
-const backendurl = 'http://localhost:8000/api'
-// const backendurl = 'https://ordorra.pxxl.xyz/api'
+// const backendurl = 'http://localhost:8000/api'
+const backendurl = 'https://ordorra.pxxl.xyz/api'
 export {backendurl}
